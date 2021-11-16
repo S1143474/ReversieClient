6870f8c973b3caa77c5a5660d559f0c70c10d5eb
Bas Schimmel <s1143474@student.windesheim.nl>
2021-11-16 23:32:39 +0100

    Add spec

---
+++ spec/add-spec.js
@@ -0,0 +1,13 @@
+describe("Addition", () => {
+    it("is 1", () => {
+        expect(1).toBe(1);
+    });
+
+    it("can add 2 to 3", () => {
+        expect(2 + 3).toBe(5);
+    });
+
+    it("can 6 minus 3", () => {
+        expect(6 - 3).toBe(3);
+    });
+});
